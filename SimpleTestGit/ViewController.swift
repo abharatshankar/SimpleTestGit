5dfdcd5170b8f8f7170b448ae66404de5debc1fd
Bharat <bharatshankar5@gmail.com>
2019-08-21 23:20:15 +0530

    First commit

---
+++ SimpleTestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTestGit
+//
+//  Created by ScriptBees on 21/08/19.
+//  Copyright © 2019 ScriptBees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
